02efa9b35711f111987742f31e5fe261389184b1
reyre808 <reyre808@hotmail.com>
2023-02-14 01:02:47 -0600

    Add project files.

---
+++ personal/Models/ContactViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace personal.Models
+{
+    public class ContactViewModel
+    {
+        //We can use DataAnnotations to add validation to our model.
+        //This is useful when we have required fields or need certain types of info.
+        [Required(ErrorMessage= "* Required")]
+        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage = "* Required")]
+        [DataType(DataType.EmailAddress, ErrorMessage= "Must be in correct @ format")]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "* Required")]
+        public string Subject { get; set; } = null!;
+
+        [Required(ErrorMessage = "* Required")]
+        [DataType(DataType.MultilineText)]//gives us a larger textbox. (textarea)
+        public string Message { get; set; } = null!;
+    }
+}
---
+++ personal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace personal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
